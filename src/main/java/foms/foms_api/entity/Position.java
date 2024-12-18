e65b42582082584c1929c56ec5c278195e0c429b
adeikmalm <adeikmalmaulanaa@gmail.com>
2024-12-18 19:14:49 +0700

    Position

--- src/main/java/foms/foms_api/entity/Position.java
+++ src/main/java/foms/foms_api/entity/Position.java
@@ -1,11 +1,18 @@
 package foms.foms_api.entity;
 
-import jakarta.persistence.*;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.List;
 
 @Getter
 @Setter
@@ -49,4 +56,4 @@ public class Position {
     private List<Officer> officers;
 }
 
-
+//tes
